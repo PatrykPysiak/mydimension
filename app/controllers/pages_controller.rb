466db7d55f123d85c01a05bf60371302039e38ca
Patryk Pysiak <patrykpysiak@gmail.com>
2022-11-03 12:07:24 +0100

    clouds added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :mydimension, :home ]
+
+  def mydimension
+    @disable_navbar = true
+  end
 
   def home
   end
